f8a9c09a0943ab811665265e579e647daa653e01
Kiabellla <scarlett.jimenez26@compscihigh.org>
2024-09-18 15:13:36 +0000

    favorite snack

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ console.log("Scarlett Jimenez");
 // Stage and commit this change with an appropriate message
 // Task 2
 // Print your favorite snack
-
+console.log("Cookies");
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
