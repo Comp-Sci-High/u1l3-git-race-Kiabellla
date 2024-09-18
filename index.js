9c480a706c2a938117e0833ffe274f6cbb5d7fea
Kiabellla <scarlett.jimenez26@compscihigh.org>
2024-09-18 15:14:26 +0000

    favorite artist

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ console.log("Cookies");
 // Stage and commit this change with an appropriate message
 // Task 3
 // Print your favorite artist
-
+console.log("A$AP ROCKY");
 // Stage and commit this change with an appropriate message
 // Go to more.js
