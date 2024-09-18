1a773dae382ce9995c4639e8613e887b644da956
Kiabellla <scarlett.jimenez26@compscihigh.org>
2024-09-18 15:16:11 +0000

    deli order

--- more.js
+++ more.js
@@ -4,6 +4,6 @@ console.log("Mc Donalds");
 // Stage and commit this change with an appropriate message
 // Task 5
 // Print your go to deli order
-
+console.log("Bacon egg and cheese no ketchup")
 // Stage and commit this change with an appropriate message
 // Go to README.md
